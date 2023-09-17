ede46b0a000c4b142b8d7377a213cb164aab31d8
kamatchip <145197197+kamatchip@users.noreply.github.com>
2023-09-17 19:35:23 +0530

    Update streamlit_app.py
    Added Pandas and readthe CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
