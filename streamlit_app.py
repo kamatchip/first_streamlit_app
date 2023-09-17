dda300d4a8ae29a0b6e075a95d9f819f658213a5
kamatchip <145197197+kamatchip@users.noreply.github.com>
2023-09-17 19:48:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents Healthy Diner')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #pick some fruits
-streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index)
+streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
