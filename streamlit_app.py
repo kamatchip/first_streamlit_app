460ad9b00384cbe99cf2dfd93c05258fc144843c
kamatchip <145197197+kamatchip@users.noreply.github.com>
2023-09-17 19:54:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ streamlit.title('My Parents Healthy Diner')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruits')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 #pick some fruits
 streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
