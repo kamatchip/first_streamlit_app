1860019b234ff59f254f4a70a8896a5f01bf35b7
kamatchip <145197197+kamatchip@users.noreply.github.com>
2023-09-19 21:18:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.title('My Parents Healthy Diner')
-
-
-
-
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
