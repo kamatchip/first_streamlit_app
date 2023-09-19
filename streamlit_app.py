551dfa7cc1c535e7bd0b0499d589bcecf64e1334
kamatchip <145197197+kamatchip@users.noreply.github.com>
2023-09-19 21:36:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header('Fruityvice Fruit Advice')
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
